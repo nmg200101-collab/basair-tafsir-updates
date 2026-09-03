391a0359df691b8e145685fa375320de9192cf88
nmg200101-collab <nmg200101@gmail.com>
2026-09-03 16:12:50 +0300

    Add Arabic Core boot stability guard

---
+++ samsung-arabic-build/app/src/main/java/com/basair/samsungarabic/ArabicCoreBootReceiver.java
@@ -0,0 +1,47 @@
+package com.basair.samsungarabic;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+public class ArabicCoreBootReceiver extends BroadcastReceiver {
+    private static final String CORE_PACKAGE = "com.samsung.Arabic_Core_Theme01";
+    private static final String THEME_CENTER = "com.samsung.android.themecenter";
+    private static final String ACTION_REAPPLY = "com.samsung.android.theme.themecenter.THEME_REAPPLY";
+
+    @Override public void onReceive(Context context, Intent intent) {
+        if (!isCoreInstalled(context)) return;
+        tryReapply(context);
+    }
+
+    private boolean isCoreInstalled(Context context) {
+        try {
+            context.getPackageManager().getPackageInfo(CORE_PACKAGE, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    private void tryReapply(Context context) {
+        try {
+            Intent i = new Intent(ACTION_REAPPLY);
+            i.setPackage(THEME_CENTER);
+            i.putExtra("packageName", CORE_PACKAGE);
+            i.putExtra("package", CORE_PACKAGE);
+            i.putExtra("themePackage", CORE_PACKAGE);
+            i.addFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+            context.sendBroadcast(i);
+        } catch (Throwable ignored) {}
+
+        try {
+            Intent i2 = new Intent(ACTION_REAPPLY);
+            i2.putExtra("packageName", CORE_PACKAGE);
+            i2.putExtra("package", CORE_PACKAGE);
+            i2.putExtra("themePackage", CORE_PACKAGE);
+            i2.addFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
+            context.sendBroadcast(i2);
+        } catch (Throwable ignored) {}
+    }
+}
